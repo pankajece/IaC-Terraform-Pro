4e6b8e6cc4caf29c64f579a5d039716621536c9f
unknown <pankaj.kumar2321@gmail.com>
2025-06-21 15:20:29 +0530

    basic resource group code with modular approh

---
+++ modules/azurerm-rg/variable.tf
@@ -0,0 +1,11 @@
+variable "rg-name-c" {
+  type = string
+  default = "pan-def-rg"
+  description = "resource group name"
+}
+
+variable "rg-location-c" {
+  type = string
+  default = "Central India"
+  description = "resource group location"
+}
